b31747a0c8764252d6e3583d7f6199211573bf70
Don Nguyen <don@aerofiler.com>
2018-08-18 12:33:22 +1000

    Updates

--- src/App.vue
+++ src/App.vue
@@ -2,51 +2,51 @@
   <div id="app">
     <div class="sports-field">
       <h1>{{ title }}</h1>
-      <img
-        class="soccer-goal"
-        src="/static/soccer-goal.png"
-        alt="Soccer goal">
-      <img
-        class="soccer-ball"
-        :class="status"
-        src="/static/soccer-ball.png"
-        alt="Soccer ball"
-        @click="shoot">
+      <goal></goal>
+      <ball></ball>
     </div>
   </div>
 </template>
 
 <script>
+import Ball from "./components/Ball.vue";
+import Goal from "./components/Goal.vue";
 export default {
-  data () {
+  components: {
+    Ball,
+    Goal
+  },
+  data() {
     return {
-      status: '',
-      title: "Let's play"
-    }
+      status: "",
+      title: "Getting started",
+      goalClass: "something-great"
+    };
   },
 
   methods: {
-    shoot () {
-      const results = [
-        'miss',
-        'score'
-      ]
+    shoot() {
+      const results = ["miss", "score"];
 
-      this.status = results[Math.floor(Math.random() * results.length)]
+      this.status = results[Math.floor(Math.random() * results.length)];
 
       setTimeout(() => {
-        this.reset()
-      }, 800)
+        this.reset();
+      }, 800);
     },
 
-    reset () {
-      this.status = 'reset'
+    reset() {
+      this.status = "reset";
       setTimeout(() => {
-        this.status = ''
-      }, 500)
+        this.status = "";
+      }, 500);
+    },
+
+    imageSrc() {
+      return "/static/soccer-goal.png";
     }
   }
-}
+};
 </script>
 
 <style>
