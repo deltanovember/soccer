5f91c1e00857b849631062b01db4c2f7471cff8f
Don Nguyen <don@aerofiler.com>
2018-08-18 12:34:36 +1000

    Components

---
+++ src/components/Ball.vue
@@ -0,0 +1,37 @@
+<template>   
+      <img
+        class="soccer-ball"
+        :class="status"
+        src="/static/soccer-ball.png"
+        alt="Soccer ball"
+        @click="shoot">
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: ""
+    };
+  },
+
+  methods: {
+    shoot() {
+      const results = ["miss", "score"];
+
+      this.status = results[Math.floor(Math.random() * results.length)];
+
+      setTimeout(() => {
+        this.reset();
+      }, 800);
+    },
+
+    reset() {
+      this.status = "reset";
+      setTimeout(() => {
+        this.status = "";
+      }, 500);
+    }
+  }
+};
+</script>
---
+++ src/components/Goal.vue
@@ -0,0 +1,18 @@
+<template>
+      <img
+        :class="goalClass"
+        class="soccer-goal"
+        src="/static/soccer-goal.png"
+        alt="Soccer goal">
+</template>
+<script>
+
+    export default {
+        data() {
+            return {
+                goalClass: "something"
+            }
+        }
+    
+    }
+</script>
